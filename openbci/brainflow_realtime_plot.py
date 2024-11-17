7a2725fb94356a2c20edc139bb1db19c286c7575
SJ <dsergiojonathan@gmail.com>
2024-11-17 01:33:50 -0600

    Working GUI with OpenBCI

--- openbci/brainflow_realtime_plot.py
+++ openbci/brainflow_realtime_plot.py
@@ -1,21 +1,43 @@
 import argparse
 import logging
-
 import pyqtgraph as pg
 from brainflow import NoiseTypes
 from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
 from brainflow.data_filter import DataFilter, FilterTypes, DetrendOperations
 from PyQt5 import QtWidgets, QtCore 
 
+import serial.tools.list_ports
+
+def find_serial_port():
+    """
+    Automatically detect the serial port for the OpenBCI board.
+    Returns the port name as a string, or raises an exception if not found.
+    """
+    ports = serial.tools.list_ports.comports()
+    for port in ports:
+        # Check for specific keywords or VID/PID
+        if 'OpenBCI' in port.description or 'usbserial' in port.device:
+            return port.device
+    raise RuntimeError("OpenBCI board not found. Ensure it is connected.")
+
 
 class Graph:
-    def __init__(self, board_shim):
-        self.board_id = board_shim.get_board_id()
-        self.board_shim = board_shim
+    def __init__(self, board_id=BoardIds.SYNTHETIC_BOARD, emg=False, serial_port=''):
+        self.params = BrainFlowInputParams()
+        self.params.serial_port = serial_port
+        self.board_id = board_id
+        self.emg = emg
+
+        # Initialize the board
+        self.board_shim = BoardShim(self.board_id, self.params)
         self.exg_channels = BoardShim.get_exg_channels(self.board_id)
         self.sampling_rate = BoardShim.get_sampling_rate(self.board_id)
+        
+        # Set up the board
+        self._setup_board()
+
         self.update_speed_ms = 50
-        self.window_size = 2 # 2 seconds of data
+        self.window_size = 2  # 2 seconds of data
         self.num_points = self.window_size * self.sampling_rate
 
         self.app = QtWidgets.QApplication([])
@@ -31,9 +53,25 @@ class Graph:
         timer.start(self.update_speed_ms)
         self.app.exec_()
 
+    def _setup_board(self):
+        """Prepares the board session and sets SRB2 configuration if needed."""
+        try:
+            self.board_shim.prepare_session()
+            config_srb2_list = []
+            if self.emg and self.board_id in (BoardIds.CYTON_BOARD, BoardIds.CYTON_DAISY_BOARD):
+                for channel in self.exg_channels:
+                    config_srb2_list.append(f"x{channel}060100")
+            # Set SRB2 configuration
+            config_srb2 = ''.join(config_srb2_list)
+            self.board_shim.config_board(config_srb2)
+            self.board_shim.start_stream(450000)
+        except BaseException as e:
+            logging.warning("Exception during board setup", exc_info=True)
+            raise e
+
     def _init_timeseries(self):
-        self.plots = list()
-        self.curves = list()
+        self.plots = []
+        self.curves = []
         for i in range(len(self.exg_channels)):
             p = self.win.addPlot(row=i, col=0)
             p.showAxis('left', False)
@@ -49,70 +87,42 @@ class Graph:
     def update(self):
         data = self.board_shim.get_current_board_data(self.num_points)
         for count, channel in enumerate(self.exg_channels):
-            # plot timeseries
+            # Detrend
             DataFilter.detrend(data[channel], DetrendOperations.CONSTANT.value)
+            
+            # Bandpass filter
+            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 4.0, 250, 2, FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
+            
+            # Notch filter
             DataFilter.remove_environmental_noise(data[channel], self.sampling_rate, NoiseTypes.FIFTY_AND_SIXTY)
-            DataFilter.perform_bandpass(data[channel], self.sampling_rate, 3.0, 45.0, 2,
-                                        FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
-            # DataFilter.perform_bandstop(data[channel], self.sampling_rate, 48.0, 52.0, 2,
-            #                             FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
-            # DataFilter.perform_bandstop(data[channel], self.sampling_rate, 58.0, 62.0, 2,
-            #                             FilterTypes.BUTTERWORTH_ZERO_PHASE, 0)
+            
+            # Update Plot
             self.curves[count].setData(data[channel].tolist())
-
+            
         self.app.processEvents()
+
+    def release_board(self):
+        """Releases board resources."""
+        if self.board_shim.is_prepared():
+            logging.info('Releasing session')
+            self.board_shim.release_session()
 
 
 def main():
     BoardShim.enable_dev_board_logger()
     logging.basicConfig(level=logging.DEBUG)
 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--timeout', type=int, help='timeout for device discovery or connection', required=False,
-                        default=0)
-    parser.add_argument('--ip-port', type=int, help='ip port', required=False, default=0)
-    parser.add_argument('--ip-protocol', type=int, help='ip protocol, check IpProtocolType enum', required=False,
-                        default=0)
-    parser.add_argument('--ip-address', type=str, help='ip address', required=False, default='')
-    parser.add_argument('--serial-port', type=str, help='serial port', required=False, default='')
-    parser.add_argument('--mac-address', type=str, help='mac address', required=False, default='')
-    parser.add_argument('--other-info', type=str, help='other info', required=False, default='')
-    parser.add_argument('--streamer-params', type=str, help='streamer params', required=False, default='')
-    parser.add_argument('--serial-number', type=str, help='serial number', required=False, default='')
-    parser.add_argument('--board-id', type=int, help='board id, check docs to get a list of supported boards',
-                        required=False, default=BoardIds.SYNTHETIC_BOARD)
-    parser.add_argument('--file', type=str, help='file', required=False, default='')
-    parser.add_argument('--master-board', type=int, help='master board id for streaming and playback boards',
-                        required=False, default=BoardIds.NO_BOARD)
-    args = parser.parse_args()
-
-    params = BrainFlowInputParams()
-    params.ip_port = args.ip_port
-    params.serial_port = args.serial_port
-    params.mac_address = args.mac_address
-    params.other_info = args.other_info
-    params.serial_number = args.serial_number
-    params.ip_address = args.ip_address
-    params.ip_protocol = args.ip_protocol
-    params.timeout = args.timeout
-    params.file = args.file
-    params.master_board = args.master_board
-
-    board_shim = BoardShim(args.board_id, params)
     
-    # print number of exg channels
-    print('Number of EXG channels: ', BoardShim.get_exg_channels(board_shim.get_board_id()))
+    serial_port = find_serial_port()
+    board_id = BoardIds.CYTON_BOARD
+    emg = True
+
     try:
-        board_shim.prepare_session()
-        board_shim.start_stream(450000, args.streamer_params)
-        Graph(board_shim)
+        graph = Graph(board_id=board_id, emg=emg, serial_port=serial_port)
     except BaseException:
         logging.warning('Exception', exc_info=True)
     finally:
         logging.info('End')
-        if board_shim.is_prepared():
-            logging.info('Releasing session')
-            board_shim.release_session()
 
 
 if __name__ == '__main__':
