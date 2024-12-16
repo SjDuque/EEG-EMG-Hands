5645b18cbd4f2252ca0447e2de557ac2deb1f25a
SJ <dsergiojonathan@gmail.com>
2024-12-15 18:48:51 -0600

    Using numpy buffer for record_ema

--- src/record_ema.py
+++ src/record_ema.py
@@ -5,89 +5,118 @@ import pandas as pd
 
 class NumpyBuffer:
     def __init__(self, init_capacity, shape, dtype):
-        self.shape = shape
-        self.dtype = dtype
-        self.buffer = np.zeros((init_capacity, *shape), dtype=dtype)
-        self.size = 0
+        self._dtype = dtype
+        self._buffer = np.zeros((init_capacity, *shape), dtype=dtype)
+        self._size = 0
 
     def __len__(self):
-        return self.size
-    
-    def capacity(self):
-        return len(self.buffer)
-    
-    def valid_index(self, idx):
-        return -self.size <= idx < self.size
-    
-    def __setitem__(self, idx, value):
-        self[idx] = value
+        return self._size
 
     def __getitem__(self, idx):
         # Handle single index
         if isinstance(idx, int):
             # Handle negative indices
             if idx < 0:
-                idx += self.size
+                idx += self._size
             # Check if index is out of bounds
-            if idx < 0 or idx >= self.size:
+            if idx < 0 or idx >= self._size:
                 raise IndexError("Index out of bounds.")
             # Return element
-            return self.buffer[idx]
+            return self._buffer[idx]
         # Handle slice
         elif isinstance(idx, slice):
-            start, stop, step = idx.indices(self.size)
+            start, stop, step = idx.indices(self._size)
             # Handle negative indices
             if start < 0:
-                start += self.size
+                start += self._size
             if stop < 0:
-                stop += self.size    
+                stop += self._size
             # Check if index is out of bounds
-            if start < 0 or start >= self.size or stop < 0 or stop > self.size:
+            if start < 0 or start >= self._size or stop < 0 or stop > self._size:
                 raise IndexError("Index out of bounds.")
             # Return sliced buffer
-            return self.buffer[start:stop:step]
+            return self._buffer[start:stop:step]
         # Handle list of indices
         elif isinstance(idx, list):
             # Handle negative indices
-            idx = [i if i >= 0 else i + self.size for i in idx]
+            idx = [i if i >= 0 else i + self._size for i in idx]
             # Check if all indices are valid
             if not all(self.valid_index(i) for i in idx):
                 raise IndexError("Index out of bounds.")
             # Return list of elements
-            return self.buffer[idx]
+            return self._buffer[idx]
         else:
             raise TypeError("Invalid index type.")
-        
+
+    @property
+    def dtype(self):
+        return self._dtype
+
+    @property
+    def size(self):
+        return self._size
+
+    @property
+    def capacity(self):
+        return len(self._buffer)
+
+    @property
+    def shape(self):
+        # Return shape with first dimension as size instead of capacity
+        return self._buffer[:self._size].shape
+
+    def valid_index(self, idx):
+        return -self._size <= idx < self._size
+
     def need_to_expand(self, new_size):
-        return new_size > self.capacity()
+        return new_size > self.capacity
 
     def expand(self, new_capacity=None):
         if new_capacity is None:
-            new_capacity = self.capacity() * 2
-        new_buffer = np.zeros((new_capacity, *self.shape), dtype=self.dtype)
-        new_buffer[:self.size] = self.buffer[:self.size]
-        self.buffer = new_buffer
+            new_capacity = self.capacity * 2
+            
+        new_buffer = np.zeros((new_capacity, *self.shape), dtype=self._dtype)
+        new_buffer[:self._size] = self._buffer[:self._size]
+        self._buffer = new_buffer
+
+    def append(self, value):
+        if self.need_to_expand(self._size + 1):
+            new_capacity = max(self.capacity * 2, self._size + 1)
+            self.expand(new_capacity)
+
+        self._buffer[self._size] = value
+        self._size += 1
 
     def extend(self, array: np.ndarray):
-        new_size = len(array) + self.size
-        if self.need_to_expand(new_size):
-            new_capacity = max(self.capacity() * 2, new_size)
-            self.expand(new_capacity)
-        self.buffer[self.size:new_size] = array
+        # Check if array shape matches buffer shape
+        if array.shape[1:] != self.shape[1:]:
+            raise ValueError("Array shape does not match buffer shape.")
+        
+        array_size = array.shape[0]
+        # Check if array is empty
+        if array_size == 0:
+            return
 
-        self.size += new_size
+        new_size = array_size + self._size
+        if self.need_to_expand(new_size):
+            new_capacity = max(self.capacity * 2, new_size)
+            self.expand(new_capacity)
+
+        self._buffer[self._size:new_size] = array
+        self._size = new_size
 
     def get_all(self):
-        data = self.buffer[:self.size].copy()
-        self.size = 0  # Clear buffer after fetching
+        data = self._buffer[:self._size].copy()
+        self._size = 0  # Clear buffer after fetching
         return data
 
 class EMARecorder:
-    def __init__(self, 
-                 exg_stream_name="filtered_exg", 
-                 angle_stream_name="FingerPercentages",
-                 csv_filename="data/exg_mp",
-                 ema_spans=(1, 2, 4, 8, 16, 32, 64)
+    def __init__(self,
+                 exg_stream_name:str ="filtered_exg",
+                 angle_stream_name:str ="FingerPercentages",
+                 csv_filename:str ="data/exg_angle",
+                 init_capacity_s:float =1024,
+                 ema_spans:tuple =(1, 2, 4, 8, 16, 32, 64)
                  ):
         """
         Initializes the recorder.
@@ -100,6 +129,7 @@ class EMARecorder:
         self.exg_stream_name = exg_stream_name
         self.angle_stream_name = angle_stream_name
         self.csv_filename = csv_filename
+        self.ema_spans = ema_spans
 
         # Resolve streams
         print("Resolving EMG (EXG) stream...")
@@ -133,33 +163,58 @@ class EMARecorder:
         self.num_angle_channels = angle_info.channel_count()
         if self.num_angle_channels <= 0:
             raise ValueError("Angle stream channel_count is not set or zero.")
-        
+
         self.update_time = min(1/self.exg_rate, 1/self.angle_rate)
 
-        # Initialize buffers
-        self.dtype = np.float32
-        self.exg_buffer = {span: [] for span in ema_spans}
-        self.exg_buffer['timestamp'] = []
-        self.ema_spans = ema_spans
-        self.angle_buffer = {'timestamp': [], 'angles': []}
-        
-        # Extract EMG Channel Span names
-        self.exg_span_names = [f"ch_{i+1}_ema_{span}" for span in self.ema_spans for i in range(self.num_exg_channels)]
-        # Extract Angle channel names
+        # Initialize buffers using NumpyBuffer
+        exg_dtype = np.float32
+        mp_dtype = np.float32
+        timestamp_dtype = np.float64
+        init_capacity_exg = int(init_capacity_s * self.exg_rate)
+        init_capacity_angle = int(init_capacity_s * self.angle_rate)
+        # EXG shape: (len(ema_spans), num_exg_channels)
+        self.exg_data_buffer = NumpyBuffer(init_capacity=init_capacity_exg, shape=(len(self.ema_spans), self.num_exg_channels), dtype=exg_dtype)
+        self.exg_timestamps_buffer = NumpyBuffer(init_capacity=init_capacity_exg, shape=(), dtype=timestamp_dtype)
+
+        # Angle shape: (num_angle_channels,)
+        self.angle_data_buffer = NumpyBuffer(init_capacity=init_capacity_angle, shape=(self.num_angle_channels,), dtype=mp_dtype)
+        self.angle_timestamps_buffer = NumpyBuffer(init_capacity=init_capacity_angle, shape=(), dtype=timestamp_dtype)
+
+        # EMG Channel Span names
+        self.exg_span_names = []
+        for i in range(self.num_exg_channels):
+            for span in self.ema_spans:
+                self.exg_span_names.append(f"ch_{i+1}_ema_{span}")
+
+        # Angle channel names
         self.mp_channel_names = ['thumb', 'index', 'middle', 'ring', 'pinky']
-            
+
         print(f'EMG Span channel names: {self.exg_span_names}')
         print(f'Angle channel names: {self.mp_channel_names}')
-        
-        # Initialize previous EMA values
-        initial_exg_sample, initial_exg_timestamp = self.exg_inlet.pull_sample()
-        self.prev_ema_values = {span: np.array(initial_exg_sample) for span in self.ema_spans}
-        
-        # Add initial sample to buffer
-        for span in self.ema_spans:
-            self.exg_buffer[span].append(initial_exg_sample)
-        self.exg_buffer['timestamp'].append(initial_exg_timestamp)
-        
+
+        self.prev_ema_values = np.zeros((len(self.ema_spans), self.num_exg_channels), dtype=exg_dtype)
+        self.exg_buffer_start = len(self.ema_spans) - 1 # Start saving data after the first EMA span
+
+    @property
+    def exg_dtype(self):
+        return self.exg_data_buffer.dtype
+
+    @property
+    def mp_dtype(self):
+        return self.angle_data_buffer.dtype
+
+    @property
+    def timestamp_dtype(self):
+        return self.exg_timestamps_buffer.dtype
+
+    @property
+    def duration_exg_s(self):
+        return self.exg_data_buffer.size / self.exg_rate
+
+    @property
+    def duration_angle_s(self):
+        return self.angle_data_buffer.size / self.angle_rate
+
     def span_to_alpha(self, span):
         return 2 / (span + 1)
 
@@ -171,97 +226,108 @@ class EMARecorder:
                 # Pull data from EMG and Angle streams
                 exg_data, exg_timestamps = self.exg_inlet.pull_chunk(timeout=0.0)
                 angle_data, angle_timestamps = self.angle_inlet.pull_chunk(timeout=0.0)
-                
-                if exg_data:
-                    # Rectify and apply exponential moving average to EMG data
-                    new_exg_buffer = {}
-                    for span in self.ema_spans:
-                        # Initialize buffer for each span
-                        new_exg_buffer[span] = []
-                        alpha = self.span_to_alpha(span)
-                        # Apply EMA to each sample
-                        for sample in exg_data:
-                            sample = np.array(sample).abs()
-                            self.prev_ema_values[span] = alpha * sample + (1 - alpha) * self.prev_ema_values[span]
-                            new_exg_buffer[span].append(self.prev_ema_values[span])
-                            
-                    # Timestamps are the same for all spans
-                    self.exg_buffer['timestamp'].extend(exg_timestamps)
-                    for span in self.ema_spans:
-                        self.exg_buffer[span].extend(new_exg_buffer[span])
 
-                if angle_data:
-                    self.angle_buffer['timestamp'].extend(angle_timestamps)
-                    self.angle_buffer['angles'].extend(angle_data)
+                if exg_data and exg_timestamps:
+                    # exg_data shape: (N, num_exg_channels)
+                    # We'll convert and apply EMA for each sample and each span
+                    exg_data = np.array(exg_data, dtype=self.exg_dtype)
+                    new_exg_samples = np.zeros((len(exg_data), len(self.ema_spans), self.num_exg_channels), dtype=self.exg_dtype)
+
+                    for i, sample in enumerate(exg_data):
+                        sample[:] = np.abs(sample)
+                        # Apply EMA for each span
+                        for s_idx, span in enumerate(self.ema_spans):
+                            alpha = self.span_to_alpha(span)
+                            self.prev_ema_values[s_idx] = alpha * sample + (1 - alpha) * self.prev_ema_values[s_idx]
+                            new_exg_samples[i, s_idx, :] = self.prev_ema_values[s_idx]
+
+                    # Append the processed exg data and timestamps
+                    self.exg_data_buffer.extend(new_exg_samples)
+                    self.exg_timestamps_buffer.extend(np.array(exg_timestamps, dtype=self.timestamp_dtype))
+
+                if angle_data and angle_timestamps:
+                    angle_data = np.array(angle_data, dtype=self.mp_dtype)
+                    # Append angle data and timestamps
+                    self.angle_data_buffer.extend(angle_data)
+                    self.angle_timestamps_buffer.extend(np.array(angle_timestamps, dtype=self.timestamp_dtype))
 
                 # Sleep briefly to prevent CPU overuse
-                time.sleep(self.update_time-0.001)
+                time_to_sleep = self.update_time - 0.001
+                if time_to_sleep > 0:
+                    time.sleep(time_to_sleep)
+
         except KeyboardInterrupt:
             print("\nKeyboard interrupt received. Preparing to save data...")
             self.save_data()
 
     def save_data(self):
         """Synchronizes all buffered data and writes to the CSV file."""
-        exg_timestamps = np.array(self.exg_buffer['timestamp'], dtype=np.float64)
-        angle_timestamps = np.array(self.angle_buffer['timestamp'], dtype=np.float64)
-
-        if exg_timestamps.size == 0 or angle_timestamps.size == 0:
+        # Get EXG data
+        if self.angle_data_buffer.size == 0 or self.exg_data_buffer.size <= self.exg_buffer_start:
             print("No data collected. Exiting without saving.")
             return
         
-        exg_samples = []
-        for span in self.ema_spans:
-            exg_samples.append(np.array(self.exg_buffer[span], dtype=self.dtype))
-        
-        # Stack all EMG samples based on timestamp
-        exg_samples = np.stack(exg_samples, axis=-1)
-        angle_samples = np.array(self.angle_buffer['angles'], dtype=self.dtype)
-        
+        exg_timestamps = self.exg_timestamps_buffer.get_all()[self.exg_buffer_start:]  # shape: (N,)
+        exg_samples = self.exg_data_buffer.get_all()[self.exg_buffer_start:]  # shape: (N, len(ema_spans), num_exg_channels)
+        self.exg_buffer_start = 0
+
+        # Get Angle data
+        angle_timestamps = self.angle_timestamps_buffer.get_all()  # shape: (M,)
+        angle_samples = self.angle_data_buffer.get_all()  # shape: (M, num_angle_channels)
+
+        # Synchronize start and end times
         start = max(exg_timestamps[0], angle_timestamps[0])
         end = min(exg_timestamps[-1], angle_timestamps[-1])
-        
+
         exg_mask = (exg_timestamps >= start) & (exg_timestamps <= end)
         angle_mask = (angle_timestamps >= start) & (angle_timestamps <= end)
-        
+
         exg_timestamps = exg_timestamps[exg_mask]
         exg_samples = exg_samples[exg_mask]
+
         angle_timestamps = angle_timestamps[angle_mask]
         angle_samples = angle_samples[angle_mask]
-        # Synchronize data based on nearest timestamps using Pandas
-        
-        exg_df = pd.DataFrame(exg_samples, index=exg_timestamps, columns=self.exg_span_names)
+
+        # exg_samples shape: (N, len(ema_spans), num_exg_channels)
+        # Need to reshape to (N, len(ema_spans)*num_exg_channels)
+        N = exg_samples.shape[0]
+        exg_samples_reshaped = exg_samples.reshape(N, len(self.ema_spans)*self.num_exg_channels)
+
+        # Create DataFrames
+        exg_df = pd.DataFrame(exg_samples_reshaped, index=exg_timestamps, columns=self.exg_span_names)
         angle_df = pd.DataFrame(angle_samples, index=angle_timestamps, columns=self.mp_channel_names)
-        # Set timestamps as timedelta type
+
+        # Set timestamps as timedelta
         exg_df.index = pd.to_timedelta(exg_df.index, unit='s')
         angle_df.index = pd.to_timedelta(angle_df.index, unit='s')
-        
-        # Merge as of the nearest timestamp based
-        tolerance = self.update_time
-        tolerance = pd.to_timedelta(tolerance, unit='s')
+
+        # Merge as-of nearest timestamp
+        tolerance = pd.to_timedelta(self.update_time, unit='s')
         merged_df = pd.merge_asof(angle_df, exg_df, left_index=True, right_index=True, direction='nearest', tolerance=tolerance)
-        
+
         # Convert timestamps to milliseconds
         merged_df.index = merged_df.index.astype(int) // 10**6
-        
         # Subtract the first timestamp to start from 0
         merged_df.index -= merged_df.index[0]
-        
-        # Name index as 'time' and reset index
+        # Name index as 'timestamp'
         merged_df.index.name = 'timestamp'
-        
-        # Save to CSV using timestamp
+
+        # Save to CSV
         file_name = f"{self.csv_filename}_{int(time.time())}.csv"
-        merged_df.to_csv(f"{file_name}")
-        
+        merged_df.to_csv(file_name)
+
         print(f"Data saved to '{file_name}'. Exiting...")
-        
+
+
 def main():
     recorder = EMARecorder(
         exg_stream_name="filtered_exg",
         angle_stream_name="FingerPercentages",
-        csv_filename="data/s_0/exg_mp",
+        csv_filename="data/s_0/exg_angle",
+        ema_spans=[1, 2, 4, 8]  # example spans
     )
     recorder.collect_data()
+
 
 if __name__ == "__main__":
     main()
